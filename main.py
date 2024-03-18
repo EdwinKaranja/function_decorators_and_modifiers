c25f3ca158885f809ab4cacb0df65d235cbd0fd2
EdwinKaranja <edwinndirangu4@gmail.com>
2024-03-18 18:59:17 +0300

    The fist time committing this

---
+++ main.py
@@ -0,0 +1,38 @@
+#Decorator that saves stored functions and recalls them in case they are run again
+import json
+
+def logger(f):
+    def wrapper(*args, **kwargs):
+        result = ""
+        #To check if the function has already been run
+        try:
+            with open("history.json", "r") as file:
+                history_data = json.load(file)
+        except FileNotFoundError:
+            history_data = []
+            with open("history.json", "w") as file:
+                json.dump(history_data, file, indent=4)
+        for i in range(len(history_data)-1):
+            if f.__name__ == history_data[i]["name"] and kwargs == history_data[i]["kwargs"] and args == history_data[i]["args"]:
+                result = history_data[i]["result"]
+
+        if result == "":
+            function_info = {
+                "name": f.__name__,
+                "args": args,
+                "kwargs": kwargs,
+                "result": f(*args, **kwargs)}
+            result = f(*args, **kwargs)
+            history(function_info)
+        return result
+    return wrapper
+
+@logger
+def adder(x, y):
+    print(int(x) + int(y))
+    return(int(x) + int(y))
+
+
+adder(55, 23)
+
+
